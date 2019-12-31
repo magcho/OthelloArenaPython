02bd5050cb2eaf79d607236c025c98bde2dadcd1
magcho <mail@magcho.com>
2019-12-31 21:43:54 +0900

    wip

--- lernBoard.py
+++ lernBoard.py
@@ -1,6 +1,6 @@
 import OthelloLogic
 from pprint import pprint
-
+import pdb
 from copy import deepcopy
 
 # user lib
@@ -43,15 +43,16 @@ def lern(board):
     maxSocreMove = moves[0]
     results = []
 
-    # パスの時
+    # 自分がパスで空いては打てる時
     if len(moves) == 0:
-        return False
+        if util.isGameEnd(deepcopy(board)):
+            evalutionTree(deepcopy(board), -1, boardSize)
 
     for move in moves:
         nextBoard = deepcopy(board)
         OthelloLogic.execute(nextBoard, move, 1, boardSize)
 
-        score = evalutionTree(deepcopy(nextBoard), 1, boardSize)
+        score = evalutionTree(deepcopy(nextBoard), -1, boardSize)
         results.append(score)
         if score > maxSocre:
             score = maxSocre
@@ -60,7 +61,7 @@ def lern(board):
     return maxSocreMove
 
 
-def evalutionTree(board, player, boardSize):
+def evalutionTree(board, player, boardSize, currentDepth=1):
     """
     
     Returns:
@@ -73,14 +74,25 @@ def evalutionTree(board, player, boardSize):
     scores = []
     for move in moves:
         nextBoard = deepcopy(board)
-        OthelloLogic.execute(nextBoard, move, player * -1, boardSize)
+        #
+        if currentDepth % 2 == 1:
+            reversedBoard = deepcopy(nextBoard)
+            OthelloLogic.getReverseboard(reversedBoard)
+            # util.printMoves(reversedBoard, moves, move)
+        else:
+            pass
+            # util.printMoves(deepcopy(nextBoard), moves, move)
+
+        OthelloLogic.execute(nextBoard, move, player, boardSize)
 
         if util.isGameEnd(nextBoard):
-            pprint(nextBoard)
-            scores.append(util.getBoardScore(nextBoard))
+            #
+            print("game end")
+            OthelloLogic.printBoard(deepcopy(nextBoard))
+            currentPlayer = 1 if currentDepth % 2 == 0 else -1
+            scores.append(util.getBoardScore(nextBoard, currentPlayer))
         else:
-            evalutionTree(nextBoard, player * -1, boardSize)
-
+            evalutionTree(nextBoard, player * -1, boardSize, currentDepth + 1)
     return 0
 
 
