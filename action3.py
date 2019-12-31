297df2c4713347b0f397444009d4fb5f39a1c6ce
magcho <mail@magcho.com>
2020-01-01 02:03:06 +0900

    [update] パスもOK

--- action3.py
+++ action3.py
@@ -2,6 +2,7 @@ import OthelloLogic
 from pprint import pprint
 import lernBoard
 import util
+import action1
 
 
 def action(board, moves):
@@ -15,4 +16,10 @@ def action(board, moves):
         move
 
     """
-    return lernBoard.lern(board)
+
+    # return lernBoard.lern(board)
+    if util.getOnBoardPieces(board) > (len(board) ** 2) - 10:
+        return lernBoard.lern(board)
+
+    else:
+        return action1.action(board, moves)
